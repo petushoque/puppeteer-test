d199688280299e5ec45ea0a2fcd5e8a3d4def3df
Alexei Kubasov <kubasovalexei@yandex.ru>
2021-07-13 09:41:13 +0700

    feat: открыть браузер, перейти по адресу, сделать скриншот

---
+++ script.js
@@ -0,0 +1,25 @@
+//подключить модуль с Puppeteer`ом
+const puppeteer = require('puppeteer'); 
+
+async function testTaxiResult(){
+    console.log('Запуск браузера');
+    const browser = await puppeteer.launch({
+        headless: false, // выключен безголовый режим
+        slowMo: 100, // замедление, чтобы отслеживать выполнение теста
+    });
+
+    console.log('Создание новой вкладки в браузере');
+    const page = await browser.newPage();
+
+    console.log('Переход по ссылке');
+    await page.goto('https://www.avito.ru/barnaul/doma_dachi_kottedzhi/dom_68_m_na_uchastke_5_sot._2197340652');
+
+    await page.screenshot({path: 'testTaxiResult.png'});
+
+    // самого теста всё ещё нет
+
+    console.log('Закрытие браузера');
+    await browser.close();
+}
+
+testTaxiResult(); 
